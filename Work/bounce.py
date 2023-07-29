9942b83d5b6c148444d87dde5e4d3725c0316905
Will Felix <willfelix7@gmail.com>
2023-07-29 10:01:27 +0100

    First Commit

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+n = 1
+while n <=10:
+    height = height * (3/5)
+    print(n, round(height, 4))
+    n = n+1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 1
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        principal = principal * (1+rate/12) - (payment+extra_payment)
+        total_paid = total_paid + (payment+extra_payment)
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+    print(months, round(total_paid, 2), round(principal, 2))
+    months = months + 1
+
+# Correct overpayment in the last month
+if principal < 0:
+    total_paid = total_paid + principal
+
+print('Total paid', total_paid)
+print('Months', months-1)
